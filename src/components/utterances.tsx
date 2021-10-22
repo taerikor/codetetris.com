8e4743dc6706eb8107593abb9e564be02624380a
taerikor <akdud153@gmail.com>
2021-10-22 18:27:24 +0900

    add utterances theme

--- src/components/utterances.tsx
+++ src/components/utterances.tsx
@@ -1,38 +1,40 @@
-import React, { createRef, useLayoutEffect } from 'react';
+import React, { createRef, useLayoutEffect } from "react"
 
-const src = 'https://utteranc.es/client.js';
+const src = "https://utteranc.es/client.js"
 
 export interface IUtterancesProps {
-  repo: string;
-  theme: string;
+  repo: string
+  theme?: string
 }
+const DARK_THEME = "photon-dark"
+const LIGHT_THEME = "github-light"
 
-const Utterances: React.FC<IUtterancesProps> = React.memo(({ repo, theme }) => {
-  const containerRef = createRef<HTMLDivElement>();
+const Utterances: React.FC<IUtterancesProps> = React.memo(({ repo }) => {
+  const containerRef = createRef<HTMLDivElement>()
 
   useLayoutEffect(() => {
-    const utterances = document.createElement('script');
-
+    const utterances = document.createElement("script")
+    const isDarkTheme = localStorage.getItem("theme") === "dark"
     const attributes = {
       src,
       repo,
-      theme,
-      'issue-term': 'pathname',
-      label: 'Comments',
-      crossOrigin: 'anonymous',
-      async: 'true',
-    };
+      theme: isDarkTheme ? DARK_THEME : LIGHT_THEME,
+      "issue-term": "pathname",
+      label: "Comments",
+      crossOrigin: "anonymous",
+      async: "true",
+    }
 
     Object.entries(attributes).forEach(([key, value]) => {
-      utterances.setAttribute(key, value);
-    });
+      utterances.setAttribute(key, value)
+    })
 
-    containerRef.current!.appendChild(utterances);
-  }, [repo]);
+    containerRef.current!.appendChild(utterances)
+  }, [repo])
 
-  return <div ref={containerRef} />;
-});
+  return <div ref={containerRef} />
+})
 
-Utterances.displayName = 'Utterances';
+Utterances.displayName = "Utterances"
 
-export default Utterances;
+export default Utterances
